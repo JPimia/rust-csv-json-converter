6e113242166b2c4d4e22f6d2ee043b7fb2ec6706
JPimia <90615229+JPimia@users.noreply.github.com>
2025-04-09 17:22:20 +0300

    Implement CLI commands for user management and CSV to JSON conversion and ensure proper handling of file operations.

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,27 @@
+use clap::error::Result;
 use serde::{Deserialize, Serialize};
 use std::io::{BufReader, BufWriter};
 use std::io;
-use std::fs::File;
+use std::fs::{File, OpenOptions};
+use clap::{Parser, Subcommand};
+
+#[derive(Parser)]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    User {
+        #[command(subcommand)]
+        action: UserAction
+    },
+    Convert {
+        input: String,
+        output: String
+    },
+}
 
 #[derive(Debug, Deserialize, Serialize)]
 struct Person {
@@ -10,27 +30,73 @@ struct Person {
     email: String
 }
 
+#[derive(Subcommand, Debug)]
+enum UserAction {
+    Create {
+        name: String,
+        age: u32,
+        email: String
+    }
+}
+
 fn csv_to_json(input_path: String, output_path: String) -> io::Result<()> {
-    let file = File::open(input_path).unwrap();
+    let file = File::open(input_path)?;
     let buf_reader = BufReader::new(file);
     let mut reader = csv::Reader::from_reader(buf_reader);
 
-    let mut records: Vec<Person> = Vec::new();
-    for result in reader.deserialize() {
-        let record: Person = result?;
-        records.push(record);
-        println!("{:?}", records);
-    }
-    // let data = records.iter().collect();
+    let records: Vec<Person> = reader.deserialize().collect::<Result<_,_>>()?;
+
     let json_file = File::create(output_path)?;
     let buf_writer = BufWriter::new(json_file);
-    let jotain = serde_json::to_writer_pretty(buf_writer, &records)?;
-    println!("jotain: {:?}", jotain);
+    serde_json::to_writer_pretty(buf_writer, &records)?;
     Ok(())
 }
 
-fn main() {
-    let input_path = String::from("example.csv");
-    let asd = csv_to_json(input_path, "jsonjsonssss.json".to_string());
-    println!("ss{:?}", asd)
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let args = Cli::parse();
+    match args.command {
+        Commands::User { action } => match action {
+            UserAction::Create { name, age, email } => {
+                let new_user = Person { name, age, email };
+                let file = OpenOptions::new()
+                    .read(true)
+                    .write(true)
+                    .create(true)
+                    .open("output.json")?;
+
+                let mut users: Vec<Person> = if file.metadata()?.len() > u64::MIN {
+                    let reader = BufReader::new(file);
+                    serde_json::from_reader(reader)?
+                } else {
+                    Vec::new()
+                };
+
+                users.push(new_user);
+                let writer = BufWriter::new(File::create("output.json")?);
+                serde_json::to_writer_pretty(writer, &users)?;
+            }
+        },
+        Commands::Convert { input, output } => {
+            csv_to_json(input, output)?;
+        }
+    }
+
+    // match args[1].as_str() {
+    //     "user" => {
+    //         match &args[2].as_str() {
+    //             &"create" => {
+    //                 println!("Creating user!")
+    //             },
+    //             _ => {
+    //                 println!("other command");
+    //             }
+    //         }
+    //     },
+    //     _ => {
+    //         println!("other commands");
+    //     }
+    // }
+
+    // csv_to_json(args.input, args.output)?;
+    Ok(())
 }
